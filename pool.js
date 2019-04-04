7de6211f1cfad77d9b39b0ca3099a79c84e2125d
qt7288 <jt3898@sina.com>
2019-04-04 17:12:09 +0800

    pool

--- pool.js
+++ pool.js
@@ -1,16 +1,11 @@
 //链接池模块
 const mysql=require('mysql');
 var pool=mysql.createPool({
-    // host     : process.env.MYSQL_HOST,
-    host     : 'http://book688.applinzi.com',
-    // port     : process.env.MYSQL_PORT,
-    port     : '3307',
-    // user     : process.env.ACCESSKEY,
-    user     : 'k35kxnzk44',
-    // password : process.env.SECRETKEY,
-    password : '2mmiwk4l2j2iliw41i51xjy3x3l411jw1kmmy0kx',
-    // database : 'app_' + process.env.APPNAME,
-    database : 'app_book688',
+    host     : process.env.MYSQL_HOST,
+    port     : process.env.MYSQL_PORT,
+    user     : process.env.ACCESSKEY,
+    password : process.env.SECRETKEY,
+    database : 'app_' + process.env.APPNAME,
 	connectionLimit:100
 	});
 module.exports=pool
