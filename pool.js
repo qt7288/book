e040fabdee25547dff83b5371753826556ec7e3c
qt7288 <jt3898@sina.com>
2019-04-04 15:23:11 +0800

    11

--- pool.js
+++ pool.js
@@ -2,10 +2,14 @@
 const mysql=require('mysql');
 var pool=mysql.createPool({
     host     : process.env.MYSQL_HOST,
-    port     : process.env.MYSQL_PORT,
-    user     : process.env.ACCESSKEY,
-    password : process.env.SECRETKEY,
-    database : 'app_' + process.env.APPNAME,
+    // port     : process.env.MYSQL_PORT,
+    port     : 3306,
+    // user     : process.env.ACCESSKEY,
+    user     : 'k35kxnzk44',
+    // password : process.env.SECRETKEY,
+    password : '2mmiwk4l2j2iliw41i51xjy3x3l411jw1kmmy0kx',
+    // database : 'app_' + process.env.APPNAME,
+    database : app_book688,
 	connectionLimit:100
 	});
 module.exports=pool
